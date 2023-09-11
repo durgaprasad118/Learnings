e0a9f90bfbb62ce0604ce99eb9e224ffbac83c45
Achana Naga Durga Prasad <108184152+durgaprasad118@users.noreply.github.com>
2023-09-11 19:11:14 +0530

    Delete JavaScript/js directory

--- JavaScript/js/bsc/variables.js
+++
@@ -1,7 +0,0 @@
-const accountId = 1445;
-let accountEmail = "hitesh@gmail.com";
-var accountPassword = "12345";
-acountCity ="Hyderabad";//can be done
-// accountId = 3;//not allowed => TypeError: Assignment to a constant variable
-console.log(accountId);
-console.table([accountEmail,accountId,accountPassword]);
--- JavaScript/js/loopsandConditions/ternary.js
+++
@@ -1,3 +0,0 @@
-let isRaining =true;
-
-isRaining?console.log("Take umberella"):console.log("no need");;
